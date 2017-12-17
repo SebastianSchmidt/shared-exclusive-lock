07ca397a20feddf4d368609a3200cc43d19f22a8
Sebastian Schmidt <email@sebastian-schmidt.info>
2017-12-17 22:50:41 +0100

    Exclusive write locks.

--- src/lock.js
+++ src/lock.js
@@ -4,6 +4,8 @@ export default class Lock {
   constructor () {
     this._status = NO_LOCK
     this._readLocks = 0
+    this._readQueue = []
+    this._writeQueue = []
   }
 
   get locked () {
@@ -12,23 +14,61 @@ export default class Lock {
 
   readLock () {
     return new Promise((resolve, reject) => {
-      this._status = READ_LOCK
-      this._readLocks++
-      resolve(once(() => releaseLock(this)))
+      const resolveReadLock = () => {
+        this._status = READ_LOCK
+        this._readLocks++
+        resolve(once(() => releaseReadLock(this)))
+      }
+
+      if (this._status !== WRITE_LOCK && this._writeQueue.length === 0) {
+        resolveReadLock()
+      } else {
+        this._readQueue.push(resolveReadLock)
+      }
+    })
+  }
+
+  writeLock () {
+    return new Promise((resolve, reject) => {
+      const resolveWriteLock = () => {
+        this._status = WRITE_LOCK
+        resolve(once(() => releaseWriteLock(this)))
+      }
+
+      if (this._status === NO_LOCK) {
+        resolveWriteLock()
+      } else {
+        this._writeQueue.push(resolveWriteLock)
+      }
     })
   }
 }
 
 const NO_LOCK = 0
 const READ_LOCK = 1
+const WRITE_LOCK = 2
 
-const releaseLock = (lock) => {
-  if (lock._status === READ_LOCK) {
-    lock._readLocks--
-    if (lock._readLocks === 0) {
+const releaseReadLock = (lock) => {
+  lock._readLocks--
+  if (lock._readLocks === 0) {
+    if (lock._writeQueue.length > 0) {
+      lock._writeQueue.shift()()
+    } else {
       lock._status = NO_LOCK
     }
+  }
+}
+
+const releaseWriteLock = (lock) => {
+  if (lock._readQueue.length > 0) {
+    const tasks = lock._readQueue
+    lock._readQueue = []
+    for (let i = 0, length = tasks.length; i < length; i++) {
+      tasks[i]()
+    }
+  } else if (lock._writeQueue.length > 0) {
+    lock._writeQueue.shift()()
   } else {
-    throw new Error('Invalid status: ' + lock._status)
+    lock._status = NO_LOCK
   }
 }
--- src/lock.spec.js
+++ src/lock.spec.js
@@ -9,45 +9,117 @@ test('is not locked after creation', () => {
   expect(lock.locked).toBe(false)
 })
 
-test('is locked when a read lock has been received', async () => {
-  await lock.readLock()
-  expect(lock.locked).toBe(true)
+describe('after read lock has been received', () => {
+  test('is locked', async () => {
+    await lock.readLock()
+    expect(lock.locked).toBe(true)
+  })
+
+  test('another read lock can be received at the same time', async () => {
+    const release1 = await lock.readLock()
+    const release2 = await lock.readLock()
+    expect(lock.locked).toBe(true)
+    expect(release1).not.toBe(release2)
+  })
+
+  test('no write lock can be received at the same time', async (done) => {
+    await lock.readLock()
+    lock.writeLock().then(() => done.fail())
+    await wait(1000)
+    done()
+  })
+
+  test('a single read lock can only be released once', async () => {
+    await lock.readLock()
+    const release = await lock.readLock()
+    release()
+    expect(lock.locked).toBe(true)
+    release()
+    expect(lock.locked).toBe(true)
+  })
+
+  describe('after read lock has been released', () => {
+    test('is no longer locked when all read locks have been released', async () => {
+      const release1 = await lock.readLock()
+      const release2 = await lock.readLock()
+      expect(lock.locked).toBe(true)
+      release1()
+      expect(lock.locked).toBe(true)
+      release2()
+      expect(lock.locked).toBe(false)
+    })
+
+    test('a write lock can be received when all read locks have been released', async (done) => {
+      const release = await lock.readLock()
+      lock.writeLock().then(() => done())
+      release()
+    })
+  })
+
+  describe('after a write lock has been queued', () => {
+    test('no further read locks can be received at the same time', async (done) => {
+      await lock.readLock()
+      lock.writeLock()
+      lock.readLock().then(() => done.fail())
+      await wait(1000)
+      done()
+    })
+
+    describe('after queued write lock has been received and released', () => {
+      test('a read lock can be received', async (done) => {
+        const release = await lock.readLock()
+        lock.writeLock().then((release) => release())
+        lock.readLock().then(() => done())
+        release()
+      })
+    })
+  })
 })
 
-test('is no longer locked when a read lock has been released', async () => {
-  const release = await lock.readLock()
-  expect(lock.locked).toBe(true)
-  release()
-  expect(lock.locked).toBe(false)
+describe('after write lock has been received', () => {
+  test('is locked', async () => {
+    await lock.writeLock()
+    expect(lock.locked).toBe(true)
+  })
+
+  test('no other write lock can be received at the same time', async (done) => {
+    await lock.writeLock()
+    lock.writeLock().then(() => done.fail())
+    await wait(1000)
+    done()
+  })
+
+  test('no read lock can be received at the same time', async (done) => {
+    await lock.writeLock()
+    lock.readLock().then(() => done.fail())
+    await wait(1000)
+    done()
+  })
+
+  describe('after write lock has been released', () => {
+    test('is no longer locked', async () => {
+      const release = await lock.writeLock()
+      expect(lock.locked).toBe(true)
+      release()
+      expect(lock.locked).toBe(false)
+    })
+
+    test('a read lock can be received', async (done) => {
+      const release = await lock.writeLock()
+      lock.readLock().then(() => done())
+      release()
+    })
+
+    test('a write lock can be received', async (done) => {
+      const release = await lock.writeLock()
+      lock.writeLock().then(() => done())
+      release()
+    })
+  })
 })
 
-test('several read locks can be received at the same time', async () => {
-  await lock.readLock()
-  await lock.readLock()
-  expect(lock.locked).toBe(true)
-})
-
-test('is no longer locked when all read locks have been released', async () => {
-  const release1 = await lock.readLock()
-  const release2 = await lock.readLock()
-  expect(lock.locked).toBe(true)
-  release1()
-  expect(lock.locked).toBe(true)
-  release2()
-  expect(lock.locked).toBe(false)
-})
-
-test('a single read lock can only be released once', async () => {
-  await lock.readLock()
-  const release = await lock.readLock()
-  release()
-  expect(lock.locked).toBe(true)
-  release()
-  expect(lock.locked).toBe(true)
-})
-
-test('release lock throws Error when lock status is invalid', async () => {
-  const release = await lock.readLock()
-  lock._status = 123
-  expect(() => release()).toThrowError('Invalid status: 123')
-})
+const wait = (milliseconds) => {
+  return new Promise((resolve, reject) => {
+    setTimeout(resolve, milliseconds)
+  })
+}
